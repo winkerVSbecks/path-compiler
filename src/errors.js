04bf6a8542f60207ab6ab106bf7119eeb054e023
Varun Vachhar <varunvachhar@gmail.com>
2016-12-10 12:20:27 -0500

    📝

--- src/errors.js
+++ src/errors.js
@@ -1,9 +1,13 @@
 const errorBox = document.querySelector('#js-error');
 
 export function throwError(msg) {
+  errorBox.classList.add('bg-washed-red', 'red');
+  errorBox.classList.remove('bg-light-gray');
   errorBox.innerHTML = `<p class="ma0">${ msg }</p>`;
 }
 
 export function clearError() {
-  throwError('');
+  errorBox.classList.remove('bg-washed-red', 'red');
+  errorBox.classList.add('bg-light-gray');
+  errorBox.innerHTML = '';
 }
